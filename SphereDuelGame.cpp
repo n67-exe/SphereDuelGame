aae03171942ba263853a19f056cf4268dc11bfc7
n67-exe <65218517+n67-exe@users.noreply.github.com>
2025-12-27 03:39:03 +0200

    DEREF and ASSERT macros

--- SphereDuelGame.cpp
+++ SphereDuelGame.cpp
@@ -14,36 +14,116 @@ using namespace std;
 #define FUNCTION_SIGNATURE __PRETTY_FUNCTION__
 #endif // _MSC_VER
 
+using cstring = const char*;
+
+// optional second parameter is the message
+#define DEREF(pointer, ...)		internal::deref_impl(pointer, cstring{__VA_ARGS__}, #pointer, __FILE__, __LINE__, __FUNCTION__, FUNCTION_SIGNATURE)
+#define ASSERT(condition, ...)	internal::assert_impl(condition, cstring{__VA_ARGS__}, #condition, __FILE__, __LINE__, __FUNCTION__, FUNCTION_SIGNATURE)
+
 namespace internal
 {
+	// C++ 14 implementation of my C++ 20 implementation based on https://stackoverflow.com/questions/81870/is-it-possible-to-print-the-name-of-a-variables-type-in-standard-c/56766138#56766138
 	template <typename T>
-	T& unwrap_impl(T* pointer, const char* file, int line, const char* function)
+	auto type_name() // auto to avoid complicated signature 
+	{
+		string function = FUNCTION_SIGNATURE, prefix, suffix;
+
+#if   defined(__clang__)
+		prefix = "auto internal::type_name() [T = ";
+		suffix = "]";
+#elif defined(__GNUC__)
+		prefix = "auto internal::type_name() [with T = ";
+		suffix = "]";
+#elif defined(_MSC_VER)
+		prefix = "auto __cdecl internal::type_name<";
+		suffix = ">(void)";
+#endif
+
+		// check that signature prefix and suffix are correct
+		bool starts_with_prefix = (function.size() >= prefix.size() && function.compare(0, prefix.size(), prefix) == 0);
+		bool ends_with_suffix = (function.size() >= suffix.size() && function.compare(function.size() - suffix.size(), suffix.size(), suffix) == 0);
+
+		ASSERT(starts_with_prefix, "Function signature must start with prefix");
+		ASSERT(ends_with_suffix, "Function signature must end with suffix");
+
+		return function.substr(prefix.size(), function.size() - prefix.size() - suffix.size()); // strip prefix and suffix
+	}
+
+	constexpr cstring filename(cstring path)
+	{
+		if (path == nullptr)
+			return path;
+
+		const char* file = path;
+
+		for (const char* p = path; *p; ++p)
+			if (*p == '/' || *p == '\\')
+				file = p + 1;
+
+		return file;
+	}
+
+	string format_message(cstring category, cstring title, cstring reason, cstring code, cstring file, long long line, cstring func_name, cstring func_sig)
+	{
+		const int offset = 12;
+
+		ostringstream message;
+
+		message << left;
+		message << setw(offset) << (category ? category : "") << " : " << (title ? title : "") << '\n';
+
+		if (reason)
+			message << setw(offset) << "  reason" << " : " << reason << '\n';
+
+		if (code)
+			message << setw(offset) << "  code" << " : " << code << '\n';
+
+		if (file)
+			message << setw(offset) << "  location" << " : " << filename(file) << ":" << line << " [" << file << "]" << '\n';
+
+		if (func_sig)
+			message << setw(offset) << "  function" << " : " << (func_name ? func_name : "") << " [" << func_sig << "]" << '\n';
+		else if (func_name)
+			message << setw(offset) << "  function" << " : " << func_name << '\n';
+
+		return message.str();
+	}
+
+	[[noreturn]]
+	void report_fatal_error(cstring title, cstring reason, cstring code, cstring file, long long line, cstring func_name, cstring func_sig)
+	{
+		clog << '\n' << format_message("FATAL ERROR", title, reason, code, file, line, func_name, func_sig) << flush;
+
+		throw runtime_error("Fatal error occured");
+	}
+
+	void report_error(cstring title, cstring reason, cstring code, cstring file, long long line, cstring func_name, cstring func_sig)
+	{
+		clog << '\n' << format_message("ERROR", title, reason, code, file, line, func_name, func_sig) << flush;
+	}
+
+	template <typename T>
+	T& deref_impl(T* pointer, cstring message, cstring code, cstring file, long long line, cstring func_name, cstring func_sig)
 	{
 		if (pointer == nullptr)
-			throw runtime_error((string)"ERROR: attempted null pointer dereference at " + file + ":" + to_string(line) + " in " + function + "(...)");
+			report_fatal_error(("Null pointer dereference [" + type_name<T*>() + "]").c_str(), message, code, file, line, func_name, func_sig);
 
 		return *pointer;
 	}
 
-
-	template <typename T>
-	const T& unwrap_impl(const T* pointer, const char* file, int line, const char* function)
+	void assert_impl(bool condition, cstring message, cstring code, cstring file, long long line, cstring func_name, cstring func_sig)
 	{
-		if (pointer == nullptr)
-			throw runtime_error((string)"ERROR: attempted null pointer dereference at " + file + ":" + to_string(line) + " in " + function + "(...)");
-
-		return *pointer;
+		if (!condition)
+			report_error("Condition not satisfied", message, code, file, line, func_name, func_sig);
 	}
 }
-
-#define UNWRAP(pointer) internal::unwrap_impl(pointer, __FILE__, __LINE__, __FUNCTION__)
 
 class Camera
 {
 public:
 	Camera(I3DEngine& engine)
 		: m_engine(engine)
-		, m_camera(UNWRAP(engine.CreateCamera(kManual)))
+		, m_camera(DEREF(engine.CreateCamera(kManual)))
 	{
 		
 	}
